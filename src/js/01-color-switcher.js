49d42d1d0fbe90e5022f74858280aa9793ea764b
yana-shkvarovskaya <shkvarovskayayana@gmail.com>
2021-10-24 18:31:29 +0300

    добавляет первое задание

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+  const startBtn = document.querySelector('button[data-start]');
+  const stopBtn = document.querySelector('button[data-stop]');
+  let timerId = null;
+  const body = document.querySelector("body");
+
+
+  startBtn.addEventListener("click", () => {
+    startBtn.disabled = true;
+    timerId = setInterval(() => {
+       body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+   
+  });
+
+  stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    startBtn.disabled = false;
+  });
 
